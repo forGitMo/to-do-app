277981d5a639d705f3a2224cf0cedaa8b85a07db
Moritz Amelang <moritz@moritzs-mbp.speedport.ip>
2020-06-22 15:15:18 +0200

    new Card

--- script.js
+++ script.js
@@ -15,22 +15,50 @@ const buttonElements = document.getElementsByClassName("btn");
 
 
 Array.from(buttonElements).forEach(element => {
-    element.addEventListener('click', function (){
-        alert('1');
-    })
+
     element.addEventListener('mouseover', function (){
-        element.style.backgroundColor = 'red';
+        element.style.backgroundColor = 'red'; 
     })
     element.addEventListener('mouseleave', function (){
         element.style.backgroundColor ="green";
     })
 });
 
-const fromButtonElements = document.getElementsByClassName('form-btn');
+document.body.onload = addElement;
 
-Array.from(fromButtonElements).forEach(element => {
-    element.addEventListener ('mouseover', function(){
-        element.style.backgroundColor ='purple';
-        element.style.color = 'white';
+function addElement () { 
+  // create a new div element 
+  var newDiv = document.createElement("div"); 
+  // and give it some content 
+  var newContent = document.createTextNode("Hi there and greetings!"); 
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);  
+
+  // add the newly created element and its content into the DOM 
+  var currentDiv = document.getElementById("div1"); 
+  document.body.insertBefore(newDiv, currentDiv); 
+}
+
+
+const todoListElements = document.getElementsByClassName("list-info");
+
+function generateTodoElement(textParameter) {
+    const liTi = document.createElement("h3");
+    liTi.classList.add('title');
+    const textEle = document.createTextNode(textParameter);
+    liTi.appendChild(textEle);
+    return liTi;
+}
+
+let counter = 0;
+
+if (buttonElements.length > 0){
+    const singleButton = buttonElements[0];
+    singleButton.addEventListener('click', function(event){
+        counter = counter + 1;
+        if (todoListElements.length){
+            todoListElements[0].appendChild(generateTodoElement('codeing ' + counter));
+        }
     })
-});
+}
+
